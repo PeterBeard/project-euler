2de472b01fcbfafbea0c6356d77acf8ec707df13
Peter Beard <peter.b.beard+github@gmail.com>
2016-02-24 21:22:23 -0500

    Add Rust solution for problem 9

---
+++ rust/problem-9/src/main.rs
@@ -0,0 +1,23 @@
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+// a2 + b2 = c2
+// 
+// For example, 32 + 42 = 9 + 16 = 25 = 52.
+// 
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+fn main() {
+    'main: for a in 1..1000 {
+        let asq = a*a;
+        for b in a..1000 {
+            let bsq = b*b;
+            for c in b..1000 {
+                let csq = c*c;
+                if a+b+c == 1000 && (asq + bsq) == csq {
+                    println!("{}^2 + {}^2 = {}^2, {}*{}*{} = {}", a, b, c, a, b, c, a*b*c);
+                    break 'main;
+                }
+            }
+        }
+    }
+}
