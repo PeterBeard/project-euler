affb6143f7e0f8b0cc6fdc56db34563a5e1c1b60
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-03 01:29:13 -0500

    Add function to get digits and tests

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,6 +6,10 @@ pub mod bigint;
 
 /// Determine whether a number is prime
 pub fn is_prime(n: i64) -> bool {
+    // Negative numbers, zero, and one are not prime
+    if n < 2 {
+        return false;
+    }
     // We don't need to look at factors > sqrt(n)
     let max_factor = ((n as f64).sqrt() as i64) + 1;
     for f in (2..max_factor).rev() {
@@ -18,6 +22,9 @@ pub fn is_prime(n: i64) -> bool {
 
 /// Calculate the sum of all proper divisors of n
 pub fn sum_of_divisors(n: u32) -> u32 {
+    if n < 2 {
+        return n
+    }
     let max_factor = (n as f32).sqrt() as u32 + 1;
     let mut sum = 1;
     for i in 2..max_factor {
@@ -29,4 +36,63 @@ pub fn sum_of_divisors(n: u32) -> u32 {
         }
     }
     sum
+}
+
+/// Split an integer into its digits
+pub fn get_digits(n: u32) -> Vec<u32> {
+    let mut mut_n = n;
+    let mut digits: Vec<u32> = Vec::new();
+    while mut_n >= 1 {
+        digits.push(mut_n % 10);
+        mut_n /= 10;
+    }
+    digits.reverse();
+    digits
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn is_2_prime() {
+        assert!(is_prime(2));
+    }
+
+    #[test]
+    fn is_13_prime() {
+        assert!(is_prime(13));
+    }
+
+    #[test]
+    fn is_1_prime() {
+        assert_eq!(false, is_prime(1));
+    }
+
+    #[test]
+    fn is_0_prime() {
+        assert_eq!(false, is_prime(0));
+    }
+
+    #[test]
+    fn sum_of_divisors_0() {
+        assert_eq!(0, sum_of_divisors(0));
+    }
+
+    #[test]
+    fn sum_of_divisors_10() {
+        assert_eq!(8, sum_of_divisors(10));
+    }
+
+    #[test]
+    fn sum_of_divisors_28() {
+        assert_eq!(28, sum_of_divisors(28));
+    }
+
+    #[test]
+    fn test_get_digits() {
+        let n = 12345;
+        let digits = vec![1,2,3,4,5];
+
+        assert_eq!(digits, get_digits(n));
+    }
 }
