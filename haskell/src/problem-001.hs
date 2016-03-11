24dafbd649bf9a1e9934b9c97fa4a2738684e2b8
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-10 19:17:54 -0500

    Add Haskell solution for problem 1

---
+++ haskell/src/problem-001.hs
@@ -0,0 +1,9 @@
+-- Copyright 2016 Peter Beard
+-- Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+--
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5,
+-- we get 3, 5, 6 and 9. The sum of these multiples is 23.
+--
+-- Find the sum of all the multiples of 3 or 5 below 1000. 
+solution = sum $ filter (\n -> mod n 3 == 0 || mod n 5 == 0) [1..999]
+main = putStrLn $ "The sum of all multiples of 3 or 5 below 1,000 is " ++ (show solution)
