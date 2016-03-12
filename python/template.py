bdf1d30ddb4bb38a6e6ee68c745334d95b8caf45
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-12 18:26:37 -0500

    Add Python solution template

---
+++ python/template.py
@@ -0,0 +1,9 @@
+"""
+Copyright 2016 Peter Beard
+Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+"""
+
+def solution():
+    return 0
+
+print("The solution is {}".format(solution()))
