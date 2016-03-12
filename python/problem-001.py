dee7039dacfddce967567eb74c89e9a2bb863352
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-12 18:26:50 -0500

    Add Python solution for problem 1

---
+++ python/problem-001.py
@@ -0,0 +1,14 @@
+"""
+Copyright 2016 Peter Beard
+Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+def solution():
+    return sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(1, 1000)))
+
+print("The sum of the multiples of 3 or 5 under 1,000 is {}".format(solution()))
