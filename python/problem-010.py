e0084fea9fb1d31a9e917021813f99a4c76fa882
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-17 12:52:06 -0400

    Add Python solution for problem 10

---
+++ python/problem-010.py
@@ -0,0 +1,14 @@
+"""
+Copyright 2016 Peter Beard
+Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+from util import is_prime
+
+def solution():
+    return sum([x for x in range(2,2000000) if is_prime(x)])
+
+print("The sum of all primes < 2,000,000 is {}".format(solution()))
