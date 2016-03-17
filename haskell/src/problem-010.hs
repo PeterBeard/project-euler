0ab374b121777db08927d8785d18985220c334c7
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-16 20:52:37 -0400

    Add Haskell solution for problem 10

---
+++ haskell/src/problem-010.hs
@@ -0,0 +1,21 @@
+-- Copyright 2016 Peter Beard
+-- Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+--
+-- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+-- 
+-- Find the sum of all the primes below two million.
+
+-- Get the square root of an Int (rounded up)
+isqrt :: Int -> Int
+isqrt = ceiling . sqrt . fromIntegral
+
+-- Determine whether an integer is prime
+isPrime :: Int -> Bool
+isPrime n
+    | n < 2 = False
+    | n == 2 = True
+    | otherwise = not $ any (\f -> mod n f == 0) [2..isqrt(n)]
+
+solution = sum $ filter isPrime [2..2000000]
+
+main = putStrLn $ "The sum of all primes < 2,000,000 is " ++ (show solution)
