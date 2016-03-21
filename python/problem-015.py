2078d099ba14486590520480a6b50a27265215a4
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-21 14:41:03 -0400

    Add Python solution for problem 15

---
+++ python/problem-015.py
@@ -0,0 +1,24 @@
+"""
+Copyright 2016 Peter Beard
+Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to
+the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+"""
+
+def binomial_c(n, k):
+    """Find the binomial coefficient (n k)"""
+    n += 1
+    coeff = (n - k) / k
+    for i in range(1, k):
+        coeff *= (n - i) / i
+    return round(coeff)
+
+def solution():
+    # The number of routes from (0, 0) to (n, k) is just (n + k n)
+    # So, the number of routes to (20, 20) is:
+    return binomial_c(40, 20)
+
+print("The number of routes through a 20x20 grid is {}".format(solution()))
