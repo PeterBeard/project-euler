6952097c3ab16016b3e0e0e50d21714b5b1bcfd1
Peter Beard <peter.b.beard+github@gmail.com>
2016-03-26 20:00:33 -0400

    Add Python solution for problem 20

---
+++ python/problem-020.py
@@ -0,0 +1,32 @@
+"""
+Copyright 2016 Peter Beard
+Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+def sum_of_digits(n):
+    """Find the sum of the digits in n"""
+    dsum = 0
+    while n > 0:
+        dsum += n % 10
+        n //= 10
+    return dsum
+
+
+def factorial(n):
+    """Calcualte n!"""
+    fact = n
+    for m in range(n-1, 1, -1):
+        fact *= m
+    return fact
+
+
+def solution():
+    return sum_of_digits(factorial(100))
+
+print("The sum of the digits of 100! is {}".format(solution()))
