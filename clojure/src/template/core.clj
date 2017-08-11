fafeaa1c6808e6632f82128c296c6d2907aef4a7
Peter Beard <peter@peterbeard.co>
2017-08-11 02:13:17 -0400

    Add Clojure problem template

---
+++ clojure/src/template/core.clj
@@ -0,0 +1,17 @@
+;; Copyright 2017 Peter Beard
+;; Distributed under the GNU GPL v2. For full terms, see the LICENSE file.
+;;
+;; Problem #n
+
+(ns template.core
+  (:gen-class))
+
+(defn solution
+  "Solution for problem n"
+  []
+  ())
+
+(defn -main
+  "Print the solution"
+  [& args]
+  (println "The solution is" (solution)))
